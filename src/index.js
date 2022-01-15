9fb201b098f54d920e40b52964db17a427ac00c5
AndreyKa <andreykashk@gmail.com>
2022-01-15 19:51:13 +0200

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    const res = [];
+    const arrLength = (Math.abs(n) + '').split('').length;
+    console.log(arrLength)
+    for (let i = arrLength - 1; i >= 0; i--) {
+        res.push((Math.abs(n) + '').split('')[i])
+    }
+
+  return +res.join('')
 }
+
+// console.log(reverse(-192))
